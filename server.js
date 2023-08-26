787909f2dad78fd41332acec2112c3a4d2787539
Jchristensen8810 <123840487+Jchristensen8810@users.noreply.github.com>
2023-08-26 15:54:45 +0000

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.static('views'));
